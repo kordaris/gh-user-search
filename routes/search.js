d9249afa43e7942aea68e8ea83ae29d698546f44
Yiannis Kordaris <john.kordaris@gugroup.com>
2020-09-10 16:29:27 +0300

    users handler + rest api

--- routes/search.js
+++ routes/search.js
@@ -1,9 +1,15 @@
-var express = require('express');
-var router = express.Router();
+const express      = require('express');
+const router       = express.Router();
+const { getUsers } = require('../helpers/gh-users.js');
 
 /* search */
-router.post('/', function(req, res, next) {
-  res.render('search', { title: 'Search results' });
+router.post('/', async function(req, res, next) {
+  const query  = req.body && req.body.search;
+  const result = await getUsers(query);
+  res.render('search', { 
+    title: 'Search results', 
+    users: result 
+  });
 });
 
 module.exports = router;
