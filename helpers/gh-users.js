aa701a67182ca3151eab9802fd634b005d2034af
Yiannis Kordaris <john.kordaris@gugroup.com>
2020-09-10 17:18:07 +0300

    print users' login

--- helpers/gh-users.js
+++ helpers/gh-users.js
@@ -1,12 +1,10 @@
 const { Octokit } = require("@octokit/core");
 
-const octokit = new Octokit({ auth: `9d646433457ef9d5819d62644cfb76ca54611890` });
+const octokit = new Octokit();
 
 const getUsers = async (q) => {
     const response = await octokit.request('GET /search/users', { q });
     return response;
 }
-
-
 
 module.exports = { getUsers };
